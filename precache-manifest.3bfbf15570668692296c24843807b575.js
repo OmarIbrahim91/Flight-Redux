85914d5a2315d3d9c591cc40fffb4499f77310b6
root <omarali43620@yahoo.com>
2020-02-10 12:13:02 +0100

    Updates

---
+++ precache-manifest.3bfbf15570668692296c24843807b575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfecf7e464a2776ab5abd847d24697d",
+    "url": "/Flight-Redux/index.html"
+  },
+  {
+    "revision": "850560f3f114ebe0120e",
+    "url": "/Flight-Redux/static/js/2.566f2764.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/Flight-Redux/static/js/2.566f2764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b682afda6444d3fad8",
+    "url": "/Flight-Redux/static/js/main.1cb06e02.chunk.js"
+  },
+  {
+    "revision": "ffddf117fca6f668cf3d",
+    "url": "/Flight-Redux/static/js/runtime-main.23d80090.js"
+  }
+]);
